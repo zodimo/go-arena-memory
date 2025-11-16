df4c8e15dde817cbe2146a71208270dbea63b11e
Jaco Labuschagne <zodimo@gmail.com>
2025-11-16 11:22:12 +0200

    hashing

--- mem/hashing.go
+++ mem/hashing.go
@@ -1,11 +1,31 @@
 package mem
 
+import "strconv"
+
 type HashElementId struct {
 	Id       uint32
 	Offset   uint32
 	BaseId   uint32
 	StringId string // to recover the string from the hash
 }
+
+type HashingOptions struct {
+	StringIdJoiner func(string, string) string
+}
+
+var DefaultHashingOptions = HashingOptions{
+	StringIdJoiner: func(a, b string) string {
+		return a + b
+	},
+}
+
+func HashingOptionsWithJoiner(joiner func(string, string) string) HashingOptions {
+	return HashingOptions{
+		StringIdJoiner: joiner,
+	}
+}
+
+type HashingOption func(*HashingOptions)
 
 type HashBuilder struct {
 	hash     uint32
@@ -27,24 +47,38 @@ func (h *HashBuilder) AddByte(data byte) *HashBuilder {
 	h.hash ^= (h.hash >> 6)
 	return h
 }
-func (h *HashBuilder) AddString(key string) *HashBuilder {
+func (h *HashBuilder) AddString(key string, options ...HashingOption) *HashBuilder {
+	opts := DefaultHashingOptions
+	for _, option := range options {
+		option(&opts)
+	}
 	stringBytes := []byte(key)
 	for _, charByte := range stringBytes {
 		h.AddByte(charByte)
 	}
-	h.stringId += key
+	h.stringId = opts.StringIdJoiner(h.stringId, key)
 	return h
 }
-func (h *HashBuilder) AddNumber(number uint32) *HashBuilder {
+func (h *HashBuilder) AddNumber(number uint32, options ...HashingOption) *HashBuilder {
+	opts := DefaultHashingOptions
+	for _, option := range options {
+		option(&opts)
+	}
 	h.hash += (number + 48)
 	h.hash += (h.hash << 10)
 	h.hash ^= (h.hash >> 6)
+
+	h.stringId = opts.StringIdJoiner(h.stringId, strconv.Itoa(int(number)))
 	return h
 }
 
-func (h *HashBuilder) AddNumbers(numbers ...uint32) *HashBuilder {
+func (h *HashBuilder) AddNumbers(numbers []uint32, options ...HashingOption) *HashBuilder {
+	opts := DefaultHashingOptions
+	for _, option := range options {
+		option(&opts)
+	}
 	for _, number := range numbers {
-		h.AddNumber(number)
+		h.AddNumber(number, options...)
 	}
 	return h
 }
@@ -64,14 +98,14 @@ func (h *HashBuilder) build() HashElementId {
 	}
 }
 
-func HashString(key string, seed uint32) HashElementId {
-	return NewHashBuilder(seed).AddString(key).build()
+func HashString(key string, seed uint32, options ...HashingOption) HashElementId {
+	return NewHashBuilder(seed).AddString(key, options...).build()
 }
 
-func HashNumber(number uint32, seed uint32) HashElementId {
-	return NewHashBuilder(seed).AddNumber(number).build()
+func HashNumber(number uint32, seed uint32, options ...HashingOption) HashElementId {
+	return NewHashBuilder(seed).AddNumber(number, options...).build()
 }
 
-func HashManyNumbers(seed uint32, numbers ...uint32) HashElementId {
-	return NewHashBuilder(seed).AddNumbers(numbers...).build()
+func HashManyNumbers(seed uint32, numbers []uint32, options ...HashingOption) HashElementId {
+	return NewHashBuilder(seed).AddNumbers(numbers, options...).build()
 }
